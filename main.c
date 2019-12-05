2601c5ebd850280d84a11e266eca29282b6080f8
Diith <48160583+226776@users.noreply.github.com>
2019-12-05 11:07:09 +0100

    Add files via upload

--- main.c
+++ main.c
@@ -13,8 +13,14 @@
 			
 #include "stm32l4xx_hal.h"
 
+#include "stm32l4xx_ll_gpio.h"
+#include "stm32l4xx_ll_bus.h"
+#include "stm32l4xx_ll_utils.h"
+#include "stm32l4xx_ll_adc.h"
+
 TIM_HandleTypeDef tim_hal = {};
 UART_HandleTypeDef huart = {};
+ADC_HandleTypeDef myAdc = {};
 
 int main(void)
 {
@@ -30,7 +36,7 @@ int main(void)
 	__HAL_RCC_TIM2_CLK_ENABLE();
 	tim_hal.Instance = TIM2;
 	tim_hal.Init.Prescaler = SystemCoreClock/1000;
-	tim_hal.Init.Period = 500;
+	tim_hal.Init.Period = 721;
 	HAL_TIM_Base_Init(&tim_hal);
 
 	HAL_TIM_Base_Start_IT(&tim_hal);
@@ -39,21 +45,57 @@ int main(void)
 	// Konfiguracja PORTA pod UART
 	__HAL_RCC_GPIOA_CLK_ENABLE();
 	GPIO_InitTypeDef gpio_uart = {};
-	gpio_uart.Mode = GPIO_AF3_USART2 | GPIO_AF7_USART2;
-	gpio_uart.Pin = GPIO_PIN_15 | GPIO_PIN_2;
+	gpio_uart.Mode = GPIO_MODE_AF_PP;
+	gpio_uart.Alternate = GPIO_AF3_USART2;
+	gpio_uart.Pin = GPIO_PIN_15;
 	HAL_GPIO_Init(GPIOA, &gpio_uart);
 
-	__HAL_RCC_USART2_IS_CLK_ENABLED();
+	// Konfiguracja PORTA pod UART
+	__HAL_RCC_GPIOA_CLK_ENABLE();
+	gpio_uart.Mode = GPIO_MODE_AF_PP;
+	gpio_uart.Alternate = GPIO_AF7_USART2;
+	gpio_uart.Pin = GPIO_PIN_2;
+	HAL_GPIO_Init(GPIOA, &gpio_uart);
+
+
+	__HAL_RCC_USART2_CLK_ENABLE();
 	huart.Instance = USART2;
 	huart.Init.BaudRate = 115200;
+	huart.Init.Mode = UART_MODE_TX;
 
-	HAL_UART_MspInit(&huart);
+	HAL_UART_Init(&huart);
+
+	uint8_t pData[3];
+	uint16_t dataSize = 1;
+	uint32_t timeout = 200;
+
+
+	// -------------------------------------------------------------
+	//						ADC HAL
+	// -------------------------------------------------------------
+
+	//ADC_CHANNEL_TEMPSENSOR
+
+	__HAL_RCC_ADC_CLK_ENABLE();
+	myAdc.Instance = ADC1;
+	myAdc.Init.ClockPrescaler = ADC_CLOCK_SYNC_PCLK_DIV4;
+	myAdc.Init.Resolution = ADC_RESOLUTION_12B;
+	HAL_ADCEx_Calibration_Start();
+
+
+
 
 	for(;;)
 	{
 		//HAL_UART_Transmit
 		HAL_GPIO_TogglePin(GPIOB, GPIO_PIN_3);
-		HAL_Delay(300UL);
+		HAL_Delay(500UL);
+
+		HAL_ADC_Start();
+		HAL_ADC_PollForConversion();
+		pData[0] = HAL_ADC_GetValue();
+		HAL_UART_Transmit(&huart, pData, dataSize, timeout);
+		HAL_ADC_Stop();
 	}
 }
 
