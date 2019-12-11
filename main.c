9fe4a3c436396d9a74018296ce989dc560529c29
Diith <48160583+226776@users.noreply.github.com>
2019-12-11 23:14:51 +0100

    UpDate LL_UART

--- main.c
+++ main.c
@@ -1,12 +1,4 @@
-/**
-  ******************************************************************************
-  * @file    main.c
-  * @author  Ac6
-  * @version V1.0
-  * @date    01-December-2013
-  * @brief   Default main function.
-  ******************************************************************************
-*/
+
 
 
 #include "stm32l4xx.h"
@@ -16,10 +8,8 @@
 #include "stm32l4xx_ll_gpio.h"
 #include "stm32l4xx_ll_bus.h"
 #include "stm32l4xx_ll_utils.h"
-#include "stm32l4xx_ll_adc.h"
+#include "stm32l4xx_ll_usart.h"
 
-TIM_HandleTypeDef tim_hal = {};
-UART_HandleTypeDef huart = {};
 ADC_HandleTypeDef myAdc = {};
 
 int main(void)
@@ -32,42 +22,42 @@ int main(void)
 	gpio_hal.Pin = GPIO_PIN_3;
 	HAL_GPIO_Init(GPIOB, &gpio_hal);
 
-	//TIM2
-	__HAL_RCC_TIM2_CLK_ENABLE();
-	tim_hal.Instance = TIM2;
-	tim_hal.Init.Prescaler = SystemCoreClock/1000;
-	tim_hal.Init.Period = 721;
-	HAL_TIM_Base_Init(&tim_hal);
 
-	HAL_TIM_Base_Start_IT(&tim_hal);
-	HAL_NVIC_EnableIRQ(TIM2_IRQn);
+	//--------------------------------------------------------------------------------------------------
+	//							UART (LL)
+	//--------------------------------------------------------------------------------------------------
+
 
 	// Konfiguracja PORTA pod UART
-	__HAL_RCC_GPIOA_CLK_ENABLE();
-	GPIO_InitTypeDef gpio_uart = {};
-	gpio_uart.Mode = GPIO_MODE_AF_PP;
-	gpio_uart.Alternate = GPIO_AF3_USART2;
-	gpio_uart.Pin = GPIO_PIN_15;
-	HAL_GPIO_Init(GPIOA, &gpio_uart);
+	__HAL_RCC_GPIOA_CLK_ENABLE();		// Do przerobienia na LL_ ??
+
+	LL_GPIO_InitTypeDef gpio_uart;
+	LL_GPIO_StructInit(&gpio_uart);
+	gpio_uart.Mode = LL_GPIO_MODE_ALTERNATE;
+	gpio_uart.Alternate = LL_GPIO_AF_3;
+	gpio_uart.Pin = LL_GPIO_PIN_15;
+	LL_GPIO_Init(GPIOA, &gpio_uart);
+
 
 	// Konfiguracja PORTA pod UART
-	__HAL_RCC_GPIOA_CLK_ENABLE();
-	gpio_uart.Mode = GPIO_MODE_AF_PP;
-	gpio_uart.Alternate = GPIO_AF7_USART2;
-	gpio_uart.Pin = GPIO_PIN_2;
-	HAL_GPIO_Init(GPIOA, &gpio_uart);
+	__HAL_RCC_GPIOA_CLK_ENABLE();		// Do przerobienia na LL_ ??
+
+	LL_GPIO_StructInit(&gpio_uart);
+	gpio_uart.Mode = LL_GPIO_MODE_ALTERNATE;
+	gpio_uart.Alternate = LL_GPIO_AF_7;
+	gpio_uart.Pin = LL_GPIO_PIN_2;
+	LL_GPIO_Init(GPIOA, &gpio_uart);
 
 
-	__HAL_RCC_USART2_CLK_ENABLE();
-	huart.Instance = USART2;
-	huart.Init.BaudRate = 115200;
-	huart.Init.Mode = UART_MODE_TX;
+	__HAL_RCC_USART2_CLK_ENABLE();		// Do przerobienia na LL_ ??
 
-	HAL_UART_Init(&huart);
+	LL_USART_InitTypeDef ll_uart;
+	LL_USART_StructInit(&ll_uart);
+	ll_uart.Init.BaudRate = 115200;
+	ll_uart.Init.Mode = LL_USART_DIRECTION_TX;
 
-	uint8_t pData[3];
-	uint16_t dataSize = 1;
-	uint32_t timeout = 200;
+	LL_USART_Init(USART2, &ll_uart);
+
 
 
 	// -------------------------------------------------------------
@@ -83,31 +73,24 @@ int main(void)
 	HAL_ADCEx_Calibration_Start();
 
 
-
+	int32_t data;
 
 	for(;;)
 	{
 		//HAL_UART_Transmit
 		HAL_GPIO_TogglePin(GPIOB, GPIO_PIN_3);
+
 		HAL_Delay(500UL);
 
-		HAL_ADC_Start();
-		HAL_ADC_PollForConversion();
-		pData[0] = HAL_ADC_GetValue();
-		HAL_UART_Transmit(&huart, pData, dataSize, timeout);
-		HAL_ADC_Stop();
-	}
-}
+		HAL_ADC_Start(&myAdc);
+		if(HAL_ADC_PollForConversion(&myAdc, 100) == HAL_OK)
+		{
+			data = HAL_ADC_GetValue(&myAdc);
+			HAL_ADC_Stop(&myAdc);
+			data2 = sensorValue * ADC_REFERENCE_VOLTAGE_MV / ADC_MAX_OUTPUT_VALUE;
 
-void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef * htim)
-{
-	if(htim->Instance == TIM6)
-	{
-		HAL_IncTick();
-	}
-	else if(htim->Instance == TIM2)
-	{
-		HAL_GPIO_TogglePin(GPIOB, GPIO_PIN_3);
+		}
+
 	}
 }
 
